ca3fec7e849b5d9817c91f684e7faf4447e8c6df
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-11-20 16:13:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { ReactComponent as Logo } from "./assets/svgs/Yetti.svg";
+import google from "./assets/images/google.png";
+import styles from "./scss/app.module.scss";
 
-function App() {
+const App = function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+      <header className={styles.header}>
+        <div className="container">
+          <Logo />
+        </div>
       </header>
-    </div>
+      <section className={styles.section__login}>
+        <form action="" className={styles.form}>
+          <h1 className={styles.form__header}>Login</h1>
+          <div className={styles.form__container}>
+            <div className={styles.form__group}>
+              <input
+                type="text"
+                className={styles.form__input}
+                placeholder="someone@gmail.com"
+              />
+            </div>
+            <div className={styles.form__group}>
+              <input
+                type="password"
+                className={styles.form__input}
+                placeholder="password"
+              />
+            </div>
+            <button className={styles.form__button} type="submit">
+              Login
+            </button>
+          </div>
+          <div className={styles.form__extra}>
+            <p className={styles.form__paragraph}>Continue with</p>
+            <button className={styles.form__link}>
+              <span className={styles.form__imageBox}>
+                <img
+                  src={google}
+                  alt="login with google"
+                  className={styles.form__image}
+                />
+              </span>
+            </button>
+          </div>
+        </form>
+      </section>{" "}
+    </Fragment>
   );
-}
+};
 
 export default App;
